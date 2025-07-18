a2693c438ce5daab51bdedecca3220b06cc2e5db
AlbertoGuadron <albertoguadron92@gmail.com>
2025-07-18 15:09:41 -0600

    arreglos

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/header"
 import Footer from "@/components/footer"
-import HomePage from "@/components/home"
 import KidRatStudioBanner from "@/components/banner"
 
 export default function Home() {
